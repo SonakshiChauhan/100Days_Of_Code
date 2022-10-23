bb9fb1f76f8b63b3e48d16aa7dc69937dbb80253
Sonakshi Chauhan <chauhansonakshi1402@gmail.com>
2022-10-23 11:32:26 +0530

    .

---
+++ com/company/find.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class find {
+    public static void main(String[] args){
+
+    }
+    static boolean find(int[] arr,int target,int index){
+        if(index==arr.length){return false;}
+        return arr[index]==target|| find(arr,target,index+1);
+    }
+}
