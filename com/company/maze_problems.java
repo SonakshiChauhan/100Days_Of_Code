e9e5d879c9d36b89154f2a33d04143d4fc16f221
Sonakshi Chauhan <chauhansonakshi1402@gmail.com>
2022-12-11 20:33:23 +0530

    .

--- com/company/maze_problems.java
+++ com/company/maze_problems.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 public class maze_problems {
     public static void main(String args[]){
     //path("",3,3);
-        pathRet("",3,3);
+     //   pathRet("",3,3);
+        boolean[][] board={
+            {true,true,true},
+            {true,false,true},
+            {true,true,true}
+        };
+        pathRestrictions("",board,0,0);
     }
 
     static int count(int r,int c){
@@ -43,5 +49,21 @@ public class maze_problems {
             list.addAll(pathRet(p+'R',r,c-1));
         }
         return list;
+    }
+    static void pathRestrictions(String p,boolean[][] maze,int r,int c){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+        if(r<maze.length-1){
+            pathRestrictions(p+'D',maze,r+1,c);
+        }
+        if(c<maze[0].length-1){
+            pathRestrictions(p+'R',maze,r,c+1);
+        }
     }
 }
